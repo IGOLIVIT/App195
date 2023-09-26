016b8d14c76f7b25f40e9c444713163096ecdd50
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2023-09-26 12:35:54 +0300

    first

--- App195/App195App.swift
+++ App195/App195App.swift
@@ -6,12 +6,38 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import ApphudSDK
+import Amplitude
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        Apphud.start(apiKey: "app_CTCwgwU5aqmE7Nwpb81RYMthYFJWjE")
+        Amplitude.instance().initializeApiKey("48adf3f19daef66c593e9495855caa22")
+        
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
 
 @main
 struct App195App: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
+        
         WindowGroup {
-            ContentView()
+            
+            NavigationView {
+                
+                ContentView()
+            }
         }
     }
 }
